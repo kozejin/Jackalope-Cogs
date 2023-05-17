00828c8ef38b88c94596725320478eae437d0d28
Kozejin <drunkenkoz@gmail.com>
2023-05-16 23:53:28 -0400

    Update jackautility.py

--- jackautility/jackautility.py
+++ jackautility/jackautility.py
@@ -1,4 +1,6 @@
+import discord
 from redbot.core import commands
+import aiohttp
 
 class JackaUtility(commands.Cog):
     """Cog for custom commands"""
@@ -12,7 +14,22 @@ class JackaUtility(commands.Cog):
         """Diablo 4 Calculator"""
 
         embed = discord.Embed()
-        embed.title = f"Diablo 4 Calculator"
-        embed.url = f"http://diablo.kozejin.dev"
+        embed.title = f"D4 Build Calculator"
+        embed.url = f"https://diablo.kozejin.dev/tools/skillcalc/"
+        embed.description = f"A build calculator for Diablo 4."
+        embed.set_image(url="https://i.imgur.com/9aSJQ9E.png")
+        embed.set_footer(text="Diablo Tools")
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.bot_has_permissions(embed_links=True)
+    async def d4map(self, ctx):
+        """Diablo 4 Map"""
+
+        embed = discord.Embed()
+        embed.title = f"Diablo 4 Map"
+        embed.url = f"https://diablo.kozejin.dev/tools/map/"
+        embed.description = f"An interactive map for Diablo 4."
+        embed.set_image(url="https://i.imgur.com/k1yBWut.png")
         embed.set_footer(text="Diablo Tools")
         await ctx.send(embed=embed)
