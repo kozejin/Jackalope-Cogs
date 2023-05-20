a04e96203cd42ef7d5e13f8ab860e1af4192eca8
Kozejin <drunkenkoz@gmail.com>
2023-05-20 19:11:54 -0400

    Update d2api.py

--- d2api/d2api.py
+++ d2api/d2api.py
@@ -21,19 +21,21 @@ class D2Scraper(commands.Cog):
         # Find the table containing the item data
         table = soup.find("table", class_="wikitable")
 
-        # Iterate over each row in the table
-        for row in table.find_all("tr")[1:]:
-            columns = row.find_all("td")
-            current_item_name = columns[0].text.strip()
+        # Check if the table exists
+        if table:
+            # Iterate over each row in the table
+            for row in table.find_all("tr")[1:]:
+                columns = row.find_all("td")
+                current_item_name = columns[0].text.strip()
 
-            # Check if the current item matches the search item
-            if current_item_name.lower() == item_name.lower():
-                item_stats = columns[1].text.strip()
+                # Check if the current item matches the search item
+                if current_item_name.lower() == item_name.lower():
+                    item_stats = columns[1].text.strip()
 
-                # Send the item details as a message
-                embed = discord.Embed(title=current_item_name, description=item_stats, color=discord.Color.green())
-                await ctx.send(embed=embed)
-                return
+                    # Send the item details as a message
+                    embed = discord.Embed(title=current_item_name, description=item_stats, color=discord.Color.green())
+                    await ctx.send(embed=embed)
+                    return
 
-        # If the item was not found, send an error message
+        # If the table or item was not found, send an error message
         await ctx.send(f"Item '{item_name}' not found in the database.")
