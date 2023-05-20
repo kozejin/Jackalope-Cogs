954008c1b56d32e93fce38539c5e5e3f6fbad81d
Kozejin <drunkenkoz@gmail.com>
2023-05-20 19:25:15 -0400

    Update d2api.py

--- d2api/d2api.py
+++ d2api/d2api.py
@@ -30,14 +30,11 @@ class D2Scraper(commands.Cog):
             if item_name_element:
                 current_item_name = item_name_element.text.strip()
                 if item_name.lower() in current_item_name.lower():
-                    item_stats_element = row.find("td", align="left")
+                    item_stats_element = row.find_next_sibling("tr").find("td")
                     if item_stats_element:
-                        item_stats = item_stats_element.find_all("font")
-                        description = ""
-                        for stat in item_stats:
-                            description += stat.text.strip() + "\n"
+                        item_stats = item_stats_element.text.strip()
                     else:
-                        description = None
+                        item_stats = None
 
                     # Find the item image
                     img_element = row.find("img")
@@ -47,9 +44,11 @@ class D2Scraper(commands.Cog):
                         item_image_url = None
 
                     # Send the item details as a message
-                    embed = discord.Embed(title=current_item_name, description=description, color=discord.Color.green())
+                    embed = discord.Embed(title=current_item_name, color=discord.Color.green())
                     if item_image_url:
                         embed.set_thumbnail(url=item_image_url)
+                    if item_stats:
+                        embed.description = item_stats
                     await ctx.send(embed=embed)
                     return
 
