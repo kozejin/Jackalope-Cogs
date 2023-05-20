bf5cf8f074f72cd1a23d042c7e9852bfccc511ff
Kozejin <drunkenkoz@gmail.com>
2023-05-20 19:14:01 -0400

    Update d2api.py

--- d2api/d2api.py
+++ d2api/d2api.py
@@ -18,24 +18,22 @@ class D2Scraper(commands.Cog):
         # Create a BeautifulSoup object to parse the HTML
         soup = BeautifulSoup(html, "html.parser")
 
-        # Find the table containing the item data
-        table = soup.find("table", class_="wikitable")
+        # Find all the item rows in the table
+        rows = soup.find_all("tr")
 
-        # Check if the table exists
-        if table:
-            # Iterate over each row in the table
-            for row in table.find_all("tr")[1:]:
-                columns = row.find_all("td")
-                current_item_name = columns[0].text.strip()
-
-                # Check if the current item matches the search item
+        # Iterate over each row and extract the item details
+        for row in rows:
+            # Find the item name and stats
+            item_name_element = row.find("b")
+            if item_name_element:
+                current_item_name = item_name_element.text.strip()
                 if current_item_name.lower() == item_name.lower():
-                    item_stats = columns[1].text.strip()
+                    item_stats = row.find("font").text.strip()
 
                     # Send the item details as a message
                     embed = discord.Embed(title=current_item_name, description=item_stats, color=discord.Color.green())
                     await ctx.send(embed=embed)
                     return
 
-        # If the table or item was not found, send an error message
+        # If the item was not found, send an error message
         await ctx.send(f"Item '{item_name}' not found in the database.")
