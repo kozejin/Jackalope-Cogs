b4739a7f6d539f5ecb55dfc2a3b75280bccae5cc
Kozejin <drunkenkoz@gmail.com>
2023-05-20 20:04:12 -0400

    Update d2api.py

--- d2api/d2api.py
+++ d2api/d2api.py
@@ -23,9 +23,6 @@ class D2Scraper(commands.Cog):
         # Find all the item rows in the table
         rows = soup.find_all("tr")
 
-        found_item = False  # Flag to check if the item is found
-        item_info = []  # List to store item details
-
         # Iterate over each row and extract the item details
         for row in rows:
             # Find the item name
@@ -33,34 +30,27 @@ class D2Scraper(commands.Cog):
             if item_name_element:
                 current_item_name = item_name_element.text.strip()
                 if item_name.lower() in current_item_name.lower():
-                    found_item = True  # Set the flag to indicate item found
-                else:
-                    found_item = False  # Set the flag to indicate item not found
-                    continue  # Skip to the next row if item not found
+                    item_info_elements = row.find_all("font", face="arial,helvetica", size="-1")
+                    item_info = []
+                    for info_element in item_info_elements:
+                        item_info.append(info_element.text.strip())
 
-            # Find the item information
-            if found_item:
-                item_info_elements = row.find_all("font", face="arial,helvetica", size="-1")
-                for info_element in item_info_elements:
-                    item_info.append(info_element.text.strip())
+                    # Find the item image
+                    img_element = row.find("img")
+                    if img_element:
+                        item_image_url = "https://classic.battle.net" + img_element["src"]
+                    else:
+                        item_image_url = None
 
-        # If the item was found, send the item details as a message
-        if found_item:
-            # Find the item image
-            img_element = soup.find("img", alt=current_item_name)
-            if img_element:
-                item_image_url = "https://classic.battle.net" + img_element["src"]
-            else:
-                item_image_url = None
+                    # Send the item details as a message
+                    embed = discord.Embed(title=current_item_name, color=discord.Color.green())
+                    if item_image_url:
+                        embed.set_thumbnail(url=item_image_url)
+                    if item_info:
+                        item_info_str = "\n".join(item_info)
+                        embed.description = item_info_str
+                    await ctx.send(embed=embed)
+                    return
 
-            # Send the item details as a message
-            embed = discord.Embed(title=current_item_name, color=discord.Color.green())
-            if item_image_url:
-                embed.set_thumbnail(url=item_image_url)
-            if item_info:
-                item_info_str = "\n".join(item_info)
-                embed.description = item_info_str
-            await ctx.send(embed=embed)
-        else:
-            # If the item was not found, send an error message
-            await ctx.send(f"Item '{item_name}' not found in the database.")
+        # If the item was not found, send an error message
+        await ctx.send(f"Item '{item_name}' not found in the database.")
